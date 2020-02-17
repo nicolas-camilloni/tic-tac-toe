6c20ef1f0cf94cde4eb8c09fc67be462a1920bbb
Thierry Khalife <thierry.khalife@laplateforme.io>
2020-02-17 13:00:43 +0100

    Update index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Tic Tac Toe</title>
+    <link rel="stylesheet" href="css/style.css">
+
+            <link rel="stylesheet" href="css/croix.css">
+            
+            <link rel="stylesheet" href="css/rond.css">
+            
+</head>
+<body>
+    <main>
+        
+       
+    </main>
+</body>
+</html>
